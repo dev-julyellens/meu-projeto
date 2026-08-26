0d3f770166fabbb34d44206330bc3a6bf02ec2a6
Julyellen Santos - W5i <july.santos@w5i.com.br>
2026-08-26 13:03:25 -0300

    feat: adiciona estado para lista de projetos no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import Header from "./componentes/Header/Header";
 import Conteudo from "./componentes/Conteudo/Conteudo";
 import Projetos from "./componentes/Projetos/Projetos";
-import Botao from "./componentes/Botao/Botao";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+    const [projetos, setProjetos] = useState([
+        {
+            link: "https://github.com/dev-julyellens/projeto-1",
+            nome: "Projeto 1",
+            imagem: "src/assets/facebook.png"
+        },
+        {
+            link: "https://github.com/dev-julyellens/projeto-2",
+            nome: "Projeto 2",
+            imagem: "src/assets/tesla.png"
+        },
+        {
+            link: "https://github.com/dev-julyellens/projeto-3",
+            nome: "Projeto 3",
+            imagem: "src/assets/vite.png"
+        }
+    ]);
     return (
         <div className="conteudo-principal">
             <Header />
             <Conteudo />
-            <Botao />
-            <Projetos />
+            <Projetos projetos={projetos}/>
             <footer className="footer">
                 <p>Feito por @dev-julyellens</p>
             </footer>
