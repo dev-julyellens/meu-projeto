373bc35eeff2c59d824bcad4273ca476df27a97b
Julyellen Santos - W5i <july.santos@w5i.com.br>
2026-08-26 13:14:49 -0300

    feat: atualiza estilo do footer e adiciona informações de contato

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,30 @@ function App() {
         {
             link: "https://github.com/dev-julyellens/projeto-1",
             nome: "Projeto 1",
-            imagem: "src/assets/facebook.png"
+            imagem: "src/assets/facebook.png",
         },
         {
             link: "https://github.com/dev-julyellens/projeto-2",
             nome: "Projeto 2",
-            imagem: "src/assets/tesla.png"
+            imagem: "src/assets/tesla.png",
         },
         {
             link: "https://github.com/dev-julyellens/projeto-3",
             nome: "Projeto 3",
-            imagem: "src/assets/vite.png"
-        }
+            imagem: "src/assets/vite.png",
+        },
     ]);
     return (
         <div className="conteudo-principal">
             <Header />
             <Conteudo />
-            <Projetos projetos={projetos}/>
+            <Projetos projetos={projetos} />
             <footer className="footer">
-                <p>Feito por @dev-julyellens</p>
+                <div>
+                    <h4>Contato</h4>
+                    <p> julyellens@gmail.com </p>
+                    <p> (71) 99999-9999 </p>
+                </div>
             </footer>
         </div>
     );
