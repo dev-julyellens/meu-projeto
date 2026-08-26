c3aa5766e7307f4ea937db39d22ee2030bcc7d6a
Julyellen Santos - W5i <july.santos@w5i.com.br>
2026-08-26 13:03:08 -0300

    feat: atualiza componente Projetos para receber lista de projetos via props

--- src/componentes/Projetos/Projetos.tsx
+++ src/componentes/Projetos/Projetos.tsx
@@ -1,22 +1,18 @@
 import "./Projetos.css";
 
-function Projetos() {
+function Projetos(props: { projetos: any[] }) {
     return (
         <section className="secao-projetos">
             <h2>Projetos</h2>
             <ul className="lista-projetos">
-                <li className="item-projeto">
-                    <a href="https://github.com/dev-julyellens/projeto-1">Projeto 1</a>
-                    <img src="src/assets/facebook.png" alt="Projeto 1" />
-                </li>
-                <li className="item-projeto">
-                    <a href="https://github.com/dev-julyellens/projeto-2">Projeto 2</a>
-                    <img src="src/assets/tesla.png" alt="Projeto 2" />
-                </li>
-                <li className="item-projeto">
-                    <a href="https://github.com/dev-julyellens/projeto-3">Projeto 3</a>
-                    <img src="src/assets/vite.png" alt="Projeto 3" />
-                </li>
+                {props.projetos.map((projeto) => (
+                    <li className="item-projeto">
+                        <a href={projeto.link} target="_blank">
+                            {projeto.nome}
+                        </a>
+                        <img src={projeto.imagem} alt={projeto.nome} />
+                    </li>
+                ))}
             </ul>
         </section>
     );
